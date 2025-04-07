ba1823c562e135da6f5c71b08f386fc19caa68dd
GITuserVasu <vasu@eprobito.com>
2025-04-06 23:07:49 -0700

    Update ML1.py

--- backend/app/ML1.py
+++ backend/app/ML1.py
@@ -8,8 +8,8 @@ from django.http import HttpResponse, JsonResponse
 # Import necessary libraries
 import pandas as pd
 import numpy as np
-##import seaborn as sns  # visualisation
-##import matplotlib.pyplot as plt  # visualisation 
+import seaborn as sns  # visualisation
+import matplotlib.pyplot as plt  # visualisation 
 
 """ import tensorflow as tf
 from tf.keras.layers import Conv2D, MaxPooling2D, Dense, Flatten
