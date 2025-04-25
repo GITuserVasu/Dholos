b0961ab7252df3590378b4055bf2c92ef4260f32
GITuserVasu <vasu@eprobito.com>
2025-04-24 21:04:03 -0700

    Update aipredictionmodels.component.ts

--- UI/src/app/component/aipredictionmodels/aipredictionmodels.component.ts
+++ UI/src/app/component/aipredictionmodels/aipredictionmodels.component.ts
@@ -407,10 +407,10 @@ export class AIpredictionmodelsComponent implements OnInit {
 
       }
       else if(this.usemap == true){
-        // coords = getpolygoncentercoords()
+        this.userlat, this.userlon = this.getLatLngCenter(latLngInDegr:any)
         //this.userlat = coords[1]
         //this.userlon = coords[0]
-        //this.string_coords = this.userlat+" "+this.userlon
+        this.string_coords = this.userlat+" "+this.userlon
 
       }
 
@@ -537,6 +537,38 @@ export class AIpredictionmodelsComponent implements OnInit {
     /*   var popup = document.getElementById("yieldPopup");
       popup.classList.toggle("show"); */
     }  
+
+    rad2degr(rad: number) { return rad * 180 / Math.PI; }
+
+    degr2rad(degr: number) { return degr * Math.PI / 180; }
+
+    getLatLngCenter(latLngInDegr:any) {
+      var LATIDX = 0;
+      var LNGIDX = 1;
+      var sumX = 0;
+      var sumY = 0;
+      var sumZ = 0;
+  
+      for (var i=0; i<latLngInDegr.length; i++) {
+          var lat = this.degr2rad(latLngInDegr[i][LATIDX]);
+          var lng = this.degr2rad(latLngInDegr[i][LNGIDX]);
+          // sum of cartesian coordinates
+          sumX += Math.cos(lat) * Math.cos(lng);
+          sumY += Math.cos(lat) * Math.sin(lng);
+          sumZ += Math.sin(lat);
+      }
+  
+      var avgX = sumX / latLngInDegr.length;
+      var avgY = sumY / latLngInDegr.length;
+      var avgZ = sumZ / latLngInDegr.length;
+  
+      // convert average x, y, z coordinate to latitude and longtitude
+      var lng = Math.atan2(avgY, avgX);
+      var hyp = Math.sqrt(avgX * avgX + avgY * avgY);
+      var lat = Math.atan2(avgZ, hyp);
+  
+      return ([this.rad2degr(lat), this.rad2degr(lng)]);
+  }
 
 }
 
