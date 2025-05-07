15753fdcdbf36cb42efa744a51f02bfa8898dd24
vasuvijay <vasuvijay@users.noreply.github.com>
2025-05-06 22:35:17 -0700

    UAS
    UAS

---
+++ UI/src/app/component/soil-water-control/soil-water-control.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-soil-water-control',
+  imports: [],
+  templateUrl: './soil-water-control.component.html',
+  styleUrl: './soil-water-control.component.css'
+})
+export class SoilWaterControlComponent {
+
+}
