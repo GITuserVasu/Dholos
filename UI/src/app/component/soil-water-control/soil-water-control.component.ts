49697378125543a2660d3dad4fabe1f89d3e976a
vasuvijay <vasuvijay@users.noreply.github.com>
2025-06-01 17:40:55 -0700

    Update soil-water-control.component.ts

--- UI/src/app/component/soil-water-control/soil-water-control.component.ts
+++ UI/src/app/component/soil-water-control/soil-water-control.component.ts
@@ -185,8 +185,8 @@ this.http.post(environment.apiUrl + 'soilwatercontrolpred', predJson).subscribe(
       console.log(this.temp_array);
       for (j = 1; j < temp_array_length; j++){
         console.log(this.temp_array[j]);
-           this.predict_array[i][j] = this.temp_array[j] ;
-           console.log(this.predict_array[i][j]);
+           this.predict_array[i-1][j-1] = this.temp_array[j] ;
+           console.log(this.predict_array[i-1][j-1]);
       }
       /* this.LCC[j] = this.temp_array[0];
       this.soil_color[j] = this.temp_array[1];
