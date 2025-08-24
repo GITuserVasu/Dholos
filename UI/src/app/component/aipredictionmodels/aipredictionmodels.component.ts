71f0488739ae4bdac1073e2f3df2ee8801eece37
vasuvijay <vasuvijay@users.noreply.github.com>
2025-08-23 23:36:58 -0700

    esri map
    esri map

--- UI/src/app/component/aipredictionmodels/aipredictionmodels.component.ts
+++ UI/src/app/component/aipredictionmodels/aipredictionmodels.component.ts
@@ -37,6 +37,16 @@ import * as Coordinate from 'ol/coordinate';
 import {fromLonLat} from 'ol/proj';
 import {useGeographic} from 'ol/proj.js';
 
+import "@arcgis/map-components/components/arcgis-map";
+import "@arcgis/map-components/components/arcgis-zoom";
+import "@arcgis/map-components/components/arcgis-search";
+import "@arcgis/map-components/components/arcgis-legend";
+
+import Graphic from "@arcgis/core/Graphic.js";
+import SimpleMarkerSymbol from "@arcgis/core/symbols/SimpleMarkerSymbol.js";
+import Point from "@arcgis/core/geometry/Point.js";
+import type { ArcgisMap } from "@arcgis/map-components/components/arcgis-map";
+
 @Component({
   selector: 'app-aipredictionmodels',
 //  imports: [ CommonModule ],
@@ -279,6 +289,35 @@ export class AIpredictionmodelsComponent implements OnInit {
   }); 
 
    }  // end createInteraction
+
+
+   arcgisViewReadyChange(event: CustomEvent): void {
+    const viewElement = event.target as ArcgisMap;
+
+    const point = new Point({
+      longitude: -118.38,
+      latitude: 33.34,
+    });
+
+    // Create a symbol for drawing the point
+    const markerSymbol = new SimpleMarkerSymbol({
+      style: "triangle",
+      size: 20,
+      color: "red",
+      outline: {
+        color: "white",
+        width: 2,
+      },
+    });
+
+    // Create a graphic and add the geometry and symbol to it
+    const pointGraphic = new Graphic({
+      geometry: point,
+      symbol: markerSymbol,
+    });
+
+    viewElement.graphics.add(pointGraphic);
+  }
 
 
 /*   createNewMap(){
